561419c192bd9afcf1be2f0cc51d720b9dc6bdde
drinkbar <nagito12396@gmail.com>
2017-11-20 15:31:51 +0900

    30분이 지나면 팝업이 뜹니다.

---
+++ DirAlram/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace DirAlram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("휴먼편지체", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(132, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "먹었다!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.Enabled = true;
+            this.Timer.Interval = 1800000;
+            this.Timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("경기천년바탕 Regular", 28F);
+            this.label1.Location = new System.Drawing.Point(135, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "밥 먹었니?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "DirAlram";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DirAlram/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace DirAlram
+{
+    public partial class Form1 : Form
+    {
+        int timeLeft;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft -= 1;
+                label1.Text = timeLeft + "초 남았당";
+            }
+            else
+            {
+                Timer.Stop();
+                label1.Text = "약 먹었니?";
+                MessageBox.Show("약을 먹으렴");
+                button1.Enabled = true;
+
+                if(button1.Enabled == true)
+                {
+                    label1.Text = "밥 먹었니?";
+                    Timer.Start();
+                }
+
+            }
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Timer.Start();
+            Timer.Interval = 1800000;
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
